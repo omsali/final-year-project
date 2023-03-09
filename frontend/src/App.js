f0a5d9b086afe7bbe6bdb5063b45b7cdf545cd45
SALI OM RAVINDRA <74290304+omsali@users.noreply.github.com>
2023-03-09 16:29:10 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Jobs />} />
+        <Route path="/jobs" element={<Jobs />} />
         <Route
           path="/createprofile"
           element={
