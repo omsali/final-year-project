293ba81116ba35ae944d59dc0fd60991764393fc
SALI OM RAVINDRA <74290304+omsali@users.noreply.github.com>
2023-06-20 22:15:57 +0530

    Updated: jobcard

--- frontend/src/Components/JobCard/JobCard.jsx
+++ frontend/src/Components/JobCard/JobCard.jsx
@@ -93,8 +93,7 @@ function JobCard({ job }) {
               isSaved(job._id)
                 ? "bg-green-600 text-white cursor-not-allowed"
                 : "cursor-pointer bg-white border border-black text-black font-semibold hover:bg-[#cee1fd] hover:text-[#0F74FF] hover:border-[#0F74FF]  transition-all delay-150"
-            }
-            `}
+            }`}
             onClick={() => saveJobHandler(job?._id)}
           >
             {isSaved(job._id) ? "Saved ✔" : "Save"}
